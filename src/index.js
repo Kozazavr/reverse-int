6b21a39f139e4a1d706ae5a7099aa3c4cbcb0b41
Kozazavr <sohee@yandex.ru>
2021-02-06 17:28:10 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+   if (n < 0) {
+       n = n * (-1);
+   }
+   let str = String(n);
+   let arr = str.split('');
+   let arrRev = arr.reverse();
+   let strRev = arrRev.join('');
+   return String(strRev);
 }
